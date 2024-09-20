a1d61147aa7b10522a41ece700d443b752797c23
Anh Chien Vu <vuanhchien003@gmail.com>
2024-09-19 20:05:56 -0400

    Adjust the default prompt to make that simple to understand

--- src/defaultPrompt.js
+++ src/defaultPrompt.js
@@ -4,7 +4,6 @@ const defaultPrompt = `Take the code from each file sent and produce a comprehen
 
 ## Structure Requirements:
 - Start with a level 1 heading (#) that serves as an appropriate title for the document. This title should not be "README.md".
-- Create a level 2 heading (##) titled "Project Structure" to outline the directory structure of the project in a tree format.
 - For each file sent, provide a separate level 2 heading (##) with the file name (do not include the relative path).
 - Under each file heading, give a detailed explanation of the file's purpose and functionality, including code snippets where relevant.
 
@@ -15,13 +14,15 @@ const defaultPrompt = `Take the code from each file sent and produce a comprehen
 - If there are any dependencies or connections between the files, explain them under the appropriate headings.
 
 ## Explain details the operations and example of how this app works and how can we use it
+- If the provided files are part of a larger project, explain how they fit into the overall project structure and functionality.
+- If they are not relate to each other, explain the purpose of each file individually.
 
 ## Example Output:
 \`\`\`markdown
 # Appropriate Title for the Document
 
-## server.js
-Explanation of server.js, highlighting its role in the project, key functionality, and any important code snippets.
+## <file_name>.<extension>
+Explanation of <file_name>.<extension>, highlighting its role in the project, key functionality, and any important code snippets.
 
 ... (continue for each file)
 \`\`\`
