90d75a49b814e2d91fbd170137234b941ba3c17d
Anh Chien Vu <vuanhchien003@gmail.com>
2024-09-26 14:59:26 -0400

    Add logic to process the files within folder, allowing process all files and folders input from user

--- src/getFileContent.js
+++ src/getFileContent.js
@@ -2,27 +2,41 @@ const path = require("path");
 const fs = require("fs");
 
 function getFileContent(files, options) {
-  // Process each file in argurment list
-  const results = files.map((file) => {
+  // Create an array to store all files
+  let allFiles = [];
+
+  // Process each file path in argurment list
+  files.forEach((file) => {
     // convert a file path to an absolute path
     const filePath = path.resolve(file);
 
     // Log debug information
     if (options.debug) {
-      process.stderr.write(`Debug: Resolving file path: ${filePath}. \n`);
+      process.stderr.write(`Debug: Processing file path: ${filePath}. \n`);
 
       if (!fs.existsSync(filePath)) {
         process.stderr.write(`Error: File not found: ${filePath}. \n`);
         process.exit(1);
       }
       if (fs.statSync(filePath).isDirectory()) {
-        process.stderr.write(`Error: Path is a directory: ${filePath}. \n`);
-        process.exit(1);
+        process.stderr.write(
+          `Debug: File path is a directory: ${filePath}. \n`
+        );
+        const directoryFiles = fs
+          .readdirSync(filePath)
+          .map((f) => path.join(filePath, f));
+        allFiles = allFiles.concat(directoryFiles);
+      } else {
+        process.stderr.write(`Debug: File path is a file: ${filePath}. \n`);
+        allFiles.push(filePath);
       }
-      process.stderr.write(`Debug: Processing file: ${filePath}. \n`);
     }
+  });
 
-    return fs.readFileSync(filePath, "utf-8");
+  // Read the content of each file
+  const results = allFiles.map((file) => {
+    process.stderr.write(`Debug: Processing file: ${file}. \n`);
+    return fs.readFileSync(file, "utf-8");
   });
   return results.join("\n");
 }
