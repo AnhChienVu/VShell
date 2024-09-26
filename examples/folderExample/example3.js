69fc694d720a1dae2992c33daa63922e356ced21
Anh Chien Vu <vuanhchien003@gmail.com>
2024-09-26 14:58:02 -0400

    Create an example folder with 2 example code within

---
+++ examples/folderExample/example3.js
@@ -0,0 +1,8 @@
+// example3.js
+// This function takes two numbers and returns their sum
+
+function add(a, b) {
+  return a + b;
+}
+
+console.log("Sum of 2 and 3 is:", add(2, 3)); // Output: 5
---
+++ examples/folderExample/example4.js
@@ -0,0 +1,5 @@
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+
+console.log("Reversed string of 'hello' is:", reverseString("hello")); // Output: 'olleh'
