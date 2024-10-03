ef8edb3e3d8262461e56fd9b8f90f6d2c3c4cf61
Mayank Pareek <mpareek365@gmail.com>
2024-10-03 13:35:19 -0400

    Add logic to search config in homeDir

--- src/server.js
+++ src/server.js
@@ -1,32 +1,43 @@
 #!/usr/bin/env node
 const { Command } = require("commander");
 const path = require("path");
+const os = require("os");
 const fs = require("fs");
 const { Groq } = require("groq-sdk");
 const toml = require("smol-toml");
 require("dotenv").config();
-const promptAI = require("./ai");
+// const promptAI = require("./ai");
 const defaultPrompt = require("./defaultPrompt");
 const getFileContent = require("./getFileContent");
 
 /**
- * Parse data from a toml configuration file
- * @param {string} filepath
- * @returns parsed configuration data
- * @throws Error if parsing operation fails
+ * Parse data from a TOML configuration file
+ * @param {string} filepath - Path to the TOML config file
+ * @returns Parsed configuration data or null if file doesn't exist
+ * @throws Error if the file can't be parsed as TOML
  */
 function loadConfig(filepath) {
   try {
     const data = fs.readFileSync(filepath, "utf-8");
     return toml.parse(data);
   } catch (err) {
-    // Output error except non-existent file error
-    if (err.code !== "ENOENT") console.error("Error:", err);
+    if (err.code === "ENOENT") {
+      // File doesn't exist, ignore and return null
+      return null;
+    } else {
+      // If the file exists but parsing fails, throw an error
+      console.error("Failed to parse the TOML config file:", err.message);
+      process.exit(1); // Exit the process with an error status
+    }
   }
 }
 
-// Load configuration from config.toml
-const config = loadConfig("../.toml");
+// Define the path to the TOML config in the home directory
+const homeDir = os.homedir();
+const configFilePath = path.join(homeDir, ".config.toml");
+
+// Load configuration from the TOML config if it exists
+const config = loadConfig(configFilePath);
 
 const program = new Command();
 
@@ -41,7 +52,7 @@ program.version(CLI_VERSION);
 program
   .option("-d, --debug", "output extra debugging")
   .option("-u, --update", "update to the latest version")
-  .option("-m, --model", "specify the model to use <model>", config.model)
+  .option("-m, --model", "specify the model to use <model>", config?.model)
   .option(
     "-t, --token-usage",
     "specify the usage of token for prompt and response"
@@ -53,7 +64,7 @@ program
     parseFloat,
     process.env.GROQ_TEMPERATURE
       ? parseFloat(process.env.GROQ_TEMPERATURE)
-      : config.temperature || 0.7 // Fallback to config or default to 0.7
+      : config?.temperature || 0.7 // Fallback to config or default to 0.7
   );
 
 // Define a command to handle file inputs
